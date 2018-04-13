7db473f2692fe381349f922a4d2206849cffb54b
Alumno ICIN <Alumno ICIN@RedesPC01>
2018-04-13 12:05:38 -0300

    nuevo_proyecto

---
+++ src/pkgswitch/Switch.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pkgswitch;
+import java.util.Scanner;
+/**
+ *
+ * @author Alumno ICIN
+ */
+
+
+
+public class Switch {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+     
+        System.out.println("escoge 1 o 2");
+        
+    }
+    
+}
