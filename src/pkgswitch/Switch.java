d3dfed5edb22a0c29f85e0eeec0c309997c5f6d7
DiegoHuanaco <Diego.huan.ram@gmail.com>
2018-04-13 12:09:47 -0300

    modifique projecto

--- src/pkgswitch/Switch.java
+++ src/pkgswitch/Switch.java
@@ -20,6 +20,7 @@ public class Switch {
     public static void main(String[] args) {
      
         System.out.println("escoge 1 o 2");
+        System.out.println("hola seba");
         
     }
     
